c8bbcd71d63bd169c841bfb748049f8f6f923a12
jcowen-Berklee <jcowen@berklee.edu>
2016-10-22 20:54:34 -0400

    Final Merge from CClark

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,35 +1,27 @@
 ﻿/*
  *Roll-A-Ball Tutorial from Unity3D.com 
-<<<<<<< HEAD
  *from LMSC-281 Roll-a-Ball modified project
  */
-=======
- */
+
 //Antonio Espinosa Holguín (Antonio Espinosa branch)
 //October 20th, 2016
 //This script has been altered so that it functions as the "level 1" script.
 //To that end the "you've won!" behaviour has been changed to a LoadScene function.
->>>>>>> maze-level
+
 
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-<<<<<<< HEAD
 
 //to include the new scene load function
 using UnityEngine.SceneManagement;
 
 
-=======
-using UnityEngine.SceneManagement;
-
->>>>>>> maze-level
 public class Player_Controller : MonoBehaviour {
 
 	private Rigidbody objectRigidbody;
 	public float push;
 	private int count;
-<<<<<<< HEAD
 
 	//JC The following allows for individual levels to set varying numbers of pickups needed to win
 	public int countToWin = 12;
@@ -39,11 +31,6 @@ public class Player_Controller : MonoBehaviour {
 	//from JCox
 	private float levelDelay = 3.0f;
 
-=======
-	public Text countText;
-	public Text winText;
-
->>>>>>> maze-level
 	// Use this for initialization
 	void Start () {
 		objectRigidbody = GetComponent<Rigidbody>();
@@ -71,7 +58,6 @@ public class Player_Controller : MonoBehaviour {
 
 	void SetCounter() {
 		countText.text = "Count: " + count.ToString();
-<<<<<<< HEAD
 		if (count >= countToWin) {
 			winText.text = "You've Won the Game!!!";
 			Invoke ("NextLevel", levelDelay);
@@ -84,16 +70,10 @@ public class Player_Controller : MonoBehaviour {
 			SceneManager.LoadScene ("Level2"); 
 		}
 		else if (Application.loadedLevelName == "Level2") {
+			SceneManager.LoadScene ("Level3"); 
+		}
+		else if (Application.loadedLevelName == "Level3") {
 			SceneManager.LoadScene ("Mini_Game_Destructor"); 
 		}
 	}
-=======
-		if (count >= 12) {
-
-			winText.text = "You Win!";
-		}
-	}
-
-
->>>>>>> maze-level
 }
