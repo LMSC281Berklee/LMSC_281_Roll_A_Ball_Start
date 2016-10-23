ac848dd43136e5aa59d2fcaa2505c634f4859de8
jcowen-Berklee <jcowen@berklee.edu>
2016-10-22 21:23:18 -0400

    prep for merge

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,17 +1,30 @@
 ﻿/*
  *Roll-A-Ball Tutorial from Unity3D.com 
+ *from LMSC-281 Roll-a-Ball modified project
  */
+
+//Antonio Espinosa Holguín (Antonio Espinosa branch)
+//October 20th, 2016
+//This script has been altered so that it functions as the "level 1" script.
+//To that end the "you've won!" behaviour has been changed to a LoadScene function.
 
 
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
+//to include the new scene load function
+using UnityEngine.SceneManagement;
 
 public class Player_Controller : MonoBehaviour {
 
 	private Rigidbody objectRigidbody;
 	private int count;
+
+	//JC The following allows for individual levels to set varying numbers of pickups needed to win
+	public int countToWin = 12;
+
+	//SRamos audio functionality
 	private AudioSource source;
 
 	public float push;	
@@ -19,15 +32,15 @@ public class Player_Controller : MonoBehaviour {
 	public Text winText;
 	public AudioClip mariocoin;
 
+	//from JCox
+	private float levelDelay = 3.0f;
 
-
+	// Use this for initialization
 	void Start () {
 		source = GetComponent<AudioSource>();
 		objectRigidbody = GetComponent<Rigidbody>();
 		count = 0;
 		SetCounter();
-
-		//source.PlayOneShot(source, 1.0f);
 	}
 	
 	// Update is called once per frame
@@ -58,10 +71,28 @@ public class Player_Controller : MonoBehaviour {
 
 	void SetCounter() {
 		countText.text = "Count: " + count.ToString();
-		if (count >= 12) {
+		if (count >= countToWin) {
 			winText.text = "You've Won the Game!!!";
+			Invoke ("NextLevel", levelDelay);
 		}
 	}
+		
+	void NextLevel () {
+
+		if (Application.loadedLevelName == "Mini_Game") {
+			SceneManager.LoadScene ("Level1"); 
+		}
+		else if (Application.loadedLevelName == "Level1") {
+			SceneManager.LoadScene ("Level2"); 
+		}
+		else if (Application.loadedLevelName == "Level2") {
+			SceneManager.LoadScene ("Level3"); 
+		}
+		else if (Application.loadedLevelName == "Level3") {
+			SceneManager.LoadScene ("Mini_Game_Destructor"); 
+		}
+	}
+}
 
 //This section is an extra jump function but instead of "jump" it became "teleport up in space then fall"
 	//void Update () {
@@ -70,5 +101,3 @@ public class Player_Controller : MonoBehaviour {
 	//	} 
 	//}
 
-
-}
